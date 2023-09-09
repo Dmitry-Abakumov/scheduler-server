5fe94e9d6771e21b1a5136772476d27e95541a6b
Dmitry-Abakumov <dm.abakumov@gmail.com>
2023-09-09 17:35:01 +0300

    Adds env configuration and db connection

--- app.ts
+++ app.ts
@@ -1,5 +1,8 @@
 import express, { Express } from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app: Express = express();
 
@@ -7,3 +10,5 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/tasks");
+
+export default app;
